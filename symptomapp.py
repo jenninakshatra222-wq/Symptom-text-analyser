7afc67b225b88b752bc5a08f2f6dcbd6f424cba3
Jenni nakshatra B <jenninakshatra222@gmail.com>
2026-04-17 19:17:43 +0530

    Update symptomapp.py

--- symptomapp.py
+++ symptomapp.py
@@ -12,7 +12,7 @@ MODEL_PATH = "./bert_symptom_model"
 LABELS_PATH = "label_classes.pkl"
 import os
 from google import genai
-client = genai.Client(api_key="AIzaSyB6tHJqgjqEQgv4gdO4TwdicJXTJDrk1T0")
+client = genai.Client(api_key="YOUR_API_KEY")
 
 for m in client.models.list():
     print(m.name)
